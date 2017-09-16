700fc6fc92c6d108cd42723ec6b2ad13453b8953
Travis Goodspeed <travis@radiantmachines.com>
2017-09-16 19:41:18 -0400

    Now using physics engine.

--- main.lua
+++ main.lua
@@ -7,74 +7,94 @@
 
 --Global variables
 platform = {}
-player = {}
+penguin = {}
  
 function love.load()
+   love.window.setMode(1024,768)
+
+   --Initialize the physics engine and callbacks.
+   world = love.physics.newWorld(0, 200, true);
+   world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+
+   persisting=0
+   
    -- This is the height and the width of the platform.
    platform.width = love.graphics.getWidth()    -- This makes the platform as wide as the whole game window.
    platform.height = love.graphics.getHeight()  -- This makes the platform as tall as the whole game window.
    
-   -- This is the coordinates where the platform will be rendered.
-   platform.x = 0                               -- This starts drawing the platform at the left edge of the game window.
-   platform.y = platform.height / 2             -- This starts drawing the platform at the very middle of the game window
+   -- This is the coordinates where the penguin character will be rendered.
+   penguin.b = love.physics.newBody(world, 400,200, "dynamic")  -- set x,y position (400,200) and let it move and hit other objects ("dynamic")
+   penguin.b:setMass(10)                                        -- make it pretty light
+   penguin.s = love.physics.newRectangleShape(64,64)                  -- give it a radius of 50
+   penguin.f = love.physics.newFixture(penguin.b, penguin.s)    -- connect body to shape
+   penguin.f:setRestitution(0.4)                                -- make it bouncy
+   penguin.f:setUserData("Penguin")
 
-    
-   -- This is the coordinates where the player character will be rendered.
-   player.x = love.graphics.getWidth() / 2   -- This sets the player at the middle of the screen based on the width of the game window. 
-   player.y = love.graphics.getHeight() / 2  -- This sets the player at the middle of the screen based on the height of the game window. 
+   --Static platform; we'll need more tha none of these.
+   platform.b = love.physics.newBody(world, 400,400, "static") -- "static" makes it not move
+   platform.s = love.physics.newRectangleShape(200,50)         -- set size to 200,50 (x,y)
+   platform.f = love.physics.newFixture(platform.b, platform.s)
+   platform.f:setUserData("Block")
    
-   -- This calls the defualt sprite and puts it in the variable called player.img.
-   player.img = love.graphics.newImage('sprites/penguin/standin.png')
-   player.speed = 200
-   player.ground = player.y     -- This makes the character land on the plaform.
-   player.y_velocity = 0        -- Whenever the character hasn't jumped yet, the Y-Axis velocity is always at 0.
-   player.jump_height = -300    -- Whenever the character jumps, he can reach this height.
-   player.gravity = -500        -- Whenever the character falls, he will descend at this rate.
+   -- This calls the defualt sprite and puts it in the variable called penguin.img.
+   penguin.img = love.graphics.newImage('sprites/penguin/standin.png')
+   penguin.speed = 200
+   penguin.ground = penguin.y     -- This makes the character land on the plaform.
+   penguin.y_velocity = 0        -- Whenever the character hasn't jumped yet, the Y-Axis velocity is always at 0.
+   penguin.jump_height = -300    -- Whenever the character jumps, he can reach this height.
+   penguin.gravity = -500        -- Whenever the character falls, he will descend at this rate.
+end
+
+function beginContact(a, b, coll)
+   x,y = coll:getNormal()
+   print(a:getUserData().." colliding with "..b:getUserData().." with a vector normal of: "..x..", "..y);
 end
  
-function love.update(dt)
-   if love.keyboard.isDown('d') then
-      -- This makes sure that the character doesn't go pass the game window's right edge.
-      if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-	 player.x = player.x + (player.speed * dt)
-      end
-   elseif love.keyboard.isDown('a') then
-      -- This makes sure that the character doesn't go pass the game window's left edge.
-      if player.x > 0 then 
-	 player.x = player.x - (player.speed * dt)
-      end
+function endContact(a, b, coll)
+   persisting=0; --No longer touching
+   print(a:getUserData().." uncolliding with "..b:getUserData());
+end
+ 
+function preSolve(a, b, coll)
+   if persisting == 0 then    -- only say when they first start touching
+      print(a:getUserData().." touching "..b:getUserData())
+   elseif persisting < 20 then    -- then just start counting
+      print(persisting)
    end
-
-
-   if love.keyboard.isDown('space') then
-      -- This is in charge of player jumping.
-      
-      -- The game checks if the player is on the ground. Remember that when the player is on the ground, Y-Axis Velocity = 0.
-      if player.y_velocity == 0 then
-	 player.y_velocity = player.jump_height    -- The player's Y-Axis Velocity is set to it's Jump Height.
-      end
-   end
-
-
-   -- This is in charge of the jump physics.
-   if player.y_velocity ~= 0 then                                      -- The game checks if player has "jumped" and left the ground.
-      player.y = player.y + player.y_velocity * dt                -- This makes the character ascend/jump.
-      player.y_velocity = player.y_velocity - player.gravity * dt -- This applies the gravity to the character.
-   end
+   persisting = persisting + 1 
+end
+ 
+function postSolve(a, b, coll, normalimpulse, tangentimpulse)
    
-   -- This is in charge of collision, making sure that the character lands on the ground.
-   if player.y > player.ground then    -- The game checks if the player has jumped.
-      player.y_velocity = 0       -- The Y-Axis Velocity is set back to 0 meaning the character is on the ground again.
-      player.y = player.ground    -- The Y-Axis Velocity is set back to 0 meaning the character is on the ground again.
+end
+
+
+function love.update(dt)
+   world:update(dt);
+
+   --Left reight movement; right takes priority.
+   if love.keyboard.isDown('d') then
+      penguin.b:applyForce(1000,0);
+   elseif love.keyboard.isDown('a') then
+      penguin.b:applyForce(-1000,0);
    end
+
+   --Jump
+   if love.keyboard.isDown('space') then
+      penguin.b:applyForce(0,-5000);
+   end
+
 end
 
 function love.draw()
    love.graphics.setColor(255, 255, 255)        -- This sets the platform color to white. (The parameters are in RGB Color format).
    
    -- The platform will now be drawn as a white rectangle while taking in the variables we declared above.
-   love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
+   --love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
 
-   -- This draws the player.
-   love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 64)
+   -- This draws the penguin.
+   love.graphics.draw(penguin.img, penguin.b:getX(), penguin.b:getY(), 0, 1, 1, 32, 32)
+
+   -- Draw the block.
+   love.graphics.polygon("line", platform.b:getWorldPoints(platform.s:getPoints()))
 end
