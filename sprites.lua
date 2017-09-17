70d02bb290f7d689ad4499d30ada47a011dccc6a
Travis Goodspeed <travis@radiantmachines.com>
2017-09-17 01:49:57 -0400

    Sprite loader cleanup code.

--- sprites.lua
+++ sprites.lua
@@ -4,24 +4,23 @@
 --Initialize all sprites.
 function sprites_init()
    penguinnoeggsprite={}
-   penguinnoeggsprite["right"]={}
-   penguinnoeggsprite["left"]={}
+   penguinnoeggsprite.right={}
+   penguinnoeggsprite.left={}
 
-   penguinnoeggsprite["right"].idle={}
-   penguinnoeggsprite["right"].idle[1]=love.graphics.newImage('sprites/penguin/right/idle/01.png');
+   penguinnoeggsprite.right.idle={}
+   penguinnoeggsprite.right.idle[1]=love.graphics.newImage('sprites/penguin/right/idle/01.png');
 
-   penguinnoeggsprite["left"].idle={}
-   penguinnoeggsprite["left"].idle[1]=love.graphics.newImage('sprites/penguin/left/idle/01.png');
+   penguinnoeggsprite.left.idle={}
+   penguinnoeggsprite.left.idle[1]=love.graphics.newImage('sprites/penguin/left/idle/01.png');
 
-
-
+   
    penguineggsprite={}
-   penguineggsprite["right"]={}
-   penguineggsprite["left"]={}
+   penguineggsprite.right={}
+   penguineggsprite.left={}
 
-   penguineggsprite["right"].idle={}
-   penguineggsprite["right"].idle[1]=love.graphics.newImage('sprites/penguinegg/right/idle/01.png');
+   penguineggsprite.right.idle={}
+   penguineggsprite.right.idle[1]=love.graphics.newImage('sprites/penguinegg/right/idle/01.png');
 
-   penguineggsprite["left"].idle={}
-   penguineggsprite["left"].idle[1]=love.graphics.newImage('sprites/penguinegg/left/idle/01.png');
+   penguineggsprite.left.idle={}
+   penguineggsprite.left.idle[1]=love.graphics.newImage('sprites/penguinegg/left/idle/01.png');
 end
