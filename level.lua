ef97e04663ec6dd6be513c5bcc9d038665d4e063
Travis Goodspeed <travis@radiantmachines.com>
2017-09-16 20:28:38 -0400

    Convenience functions for level design.

--- level.lua
+++ level.lua
@@ -3,32 +3,31 @@ function init_level()
    create_edge_walls(1024,768);
 end
 
+
+
+--Generates a block from a top coordinate to a bottom right coordinate.
+function newblock(tx,ty,bx,by,name)
+   width=math.abs(bx-tx);
+   height=math.abs(by-ty);
+   x=math.min(tx,bx)+width/2
+   y=math.min(ty,by)+height/2;
+
+   --Block
+   right={}
+   right.b = love.physics.newBody(world, x, y, "static")
+   right.s = love.physics.newRectangleShape(width,height)
+   right.f = love.physics.newFixture(right.b, right.s)
+   right.f:setUserData(name)
+end
+
+
 function create_edge_walls(width,height)
    --Bottom of the world.
-   bottom={}
-   bottom.b = love.physics.newBody(world, width/2, height, "static")
-   bottom.s = love.physics.newRectangleShape(width,1)
-   bottom.f = love.physics.newFixture(bottom.b, bottom.s)
-   bottom.f:setUserData("Ground")
-
+   newblock(0,height,width,height+1,"Block");
    --Top of the world.
-   top={}
-   top.b = love.physics.newBody(world, width/2, 0, "static")
-   top.s = love.physics.newRectangleShape(width,1)
-   top.f = love.physics.newFixture(top.b, top.s)
-   top.f:setUserData("Top")
-
-   --Left of the world.
-   left={}
-   left.b = love.physics.newBody(world, 0, height/2, "static")
-   left.s = love.physics.newRectangleShape(1,height)
-   left.f = love.physics.newFixture(left.b, left.s)
-   left.f:setUserData("Left")
-
-   --Right of the world.
-   right={}
-   right.b = love.physics.newBody(world, width, height/2, "static")
-   right.s = love.physics.newRectangleShape(1,height)
-   right.f = love.physics.newFixture(right.b, right.s)
-   right.f:setUserData("Right")
+   newblock(0,0,width,1,"Block");
+   --Left
+   newblock(0,0,1,height,"Left");
+   --Right
+   newblock(width,0,width+1,height,"Right");
 end
